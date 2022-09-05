71fc92873a49ec269d74ad095a548092aeb7abf2
linhui-one <71432270+linhui-one@users.noreply.github.com>
2022-09-05 20:31:31 +0800

    Update Test.java

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,7 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("4445");
-        System.out.println("123");
+        System.out.println("444");
         System.out.println("3434");
         System.out.println("xiaoming");
         //135
