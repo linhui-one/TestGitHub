5694612439a525601b5ccebe8ef0fd296afd1023
linhui-one <1379586460@qq.com>
2022-09-07 22:48:58 +0800

    新的添加1

--- src/main/java/Test2.java
+++ src/main/java/Test2.java
@@ -1,12 +1,21 @@
+import redis.clients.jedis.Jedis;
+
+import java.util.Set;
+
 public class Test2 {
     public static void main(String[] args) {
-        System.out.println("bushi ");
-        System.out.println("d'fsf's");
-        System.out.println(""
-        );
+        Jedis jedis = new Jedis("127.0.0.1",6379);
+        String ping = jedis.ping();
+        System.out.println(ping);
 
-        System.out.println();
+//        Set<String> keys = jedis.keys("*");
+//        for (String key : keys) {
+//            System.out.println(key);
+//        }
 
-
+//        jedis.flushDB();
+        jedis.set("k2","小红");
+        String k2 = jedis.get("k2");
+        System.out.println(k2);
     }
 }
